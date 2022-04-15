f61fe7b3ef15dc8f0f72ddf02c8f6a0bc4d36fb4
Niklas Tomas <niklas.thomas@hotmail.com>
2022-04-15 11:06:36 +0200

    add auth

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly TokenService _tokenService;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+
+        TokenService tokenService)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (result.Succeeded)
+            {
+                return Ok(new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = _tokenService.CreateToken(user),
+                    Username = user.UserName
+                });
+            }
+            return Unauthorized();
+        }
+
+    }
+}
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,30 +7,24 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
+  const { activityStore } = useStore();
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<
     Activity | undefined
   >(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
     const fetchActivities = async () => {
-      agent.Activities.list().then((response) => {
-        let activities: Activity[] = [];
-        response.forEach((activity) => {
-          activity.date = activity.date.split('T')[0];
-          activities.push(activity);
-        });
-        setActivities(activities);
-        setLoading(false);
-      });
+      activityStore.loadActivities();
     };
     fetchActivities();
-  }, []);
+  }, [activityStore]);
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.find((x) => x.id === id));
   };
@@ -76,14 +70,15 @@ function App() {
     });
   };
 
-  if (loading) return <LoadingComponent content='Loading app' />;
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content='Loading app' />;
 
   return (
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelActivity={handleCancelSelectActivity}
@@ -99,4 +94,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
